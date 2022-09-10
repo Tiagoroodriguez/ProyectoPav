ee06c0db6d37c31772444593b66420858e575069
Tiagoroodriguez <110980693+Tiagoroodriguez@users.noreply.github.com>
2022-09-10 15:46:16 -0300

    Add files via upload

---
+++ ProyectoFinalPav/ProyectoPAV/ProyectoPAV/Presentacion/frmLogin.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ProyectoPAV.Presentacion
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(84, 42);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(66, 80);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(64, 13);
+            this.lblClave.TabIndex = 1;
+            this.lblClave.Text = "Contraseña:";
+            this.lblClave.Click += new System.EventHandler(this.lblClave_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(147, 39);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(150, 20);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(147, 77);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(150, 20);
+            this.txtClave.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(147, 120);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(69, 23);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(228, 120);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(69, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 180);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ ProyectoFinalPav/ProyectoPAV/ProyectoPAV/Presentacion/frmUsuario.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProyectoPAV.Servicios;
+
+
+
+namespace ProyectoPAV.Presentacion
+{
+    public partial class frmUsuario : Form
+    {
+        enum Acciones
+        {
+            Alta,
+            Modificacion,
+            Baja
+        }
+        Acciones MiAccion;
+        PerfilService oPerfil = new PerfilService();
+        UsuarioService oUsuario = new UsuarioService();
+
+        public frmUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            CargarCombo(cboPerfil, oPerfil.traerTodos());
+            CargarGrilla(grdUsuario, oUsuario.traerTodos());
+            HabilitarModoEdicion(false);
+            MiAccion = Acciones.Modificacion;
+
+        }
+        // Metodo para habilitar y deshabilitar los text box y los botones apenas se ejecuta
+        private void HabilitarModoEdicion(bool v)
+        {
+            // no se pone porque el id se genera automaticamente y el usuario no deberia poder cambiarlo.
+            //txtId.Enabled = v;
+            txtClave.Enabled = v;
+            txtMail.Enabled = v;
+            txtNombre.Enabled = v;
+            cboPerfil.Enabled = v;
+            btnGuardarU.Enabled = v;
+            btnCancelarU.Enabled = v;
+            // Niega el valor de v -> si v es true toma el valor de false
+            btnAgregarU.Enabled = !v;
+            btnEditarU.Enabled = !v;
+            btnSalirU.Enabled = !v;
+            grdUsuario.Enabled = !v;
+        }
+        // Pone todos los text box en blanco
+        private void LimpiarCampos()
+        {
+            txtIdUsuario.Text = String.Empty;
+            txtMail.Text = String.Empty;
+            txtNombre.Text = String.Empty;
+            txtClave.Text = String.Empty;
+            cboPerfil.SelectedIndex = -1;
+        }
+
+        private void CargarCombo(ComboBox combo, DataTable tabla)
+        {
+            combo.DataSource = tabla;
+            combo.DisplayMember = tabla.Columns[1].ColumnName;
+            combo.ValueMember = tabla.Columns[0].ColumnName;
+            combo.SelectedIndex = -1;
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void CargarGrilla(DataGridView grilla, DataTable tabla)
+        {
+            grilla.Rows.Clear();
+            for (int i = 0; i < tabla.Rows.Count; i++)
+            {
+                grilla.Rows.Add(tabla.Rows[i]["id_usuario"],
+                                tabla.Rows[i]["usuario"],
+                                tabla.Rows[i]["email"]);
+            }
+        }
+        private void btnAgregarU_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
